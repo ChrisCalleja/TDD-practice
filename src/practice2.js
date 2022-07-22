dff5a5c6fb59678115e2d721ec4d48b3d2852b37
Chris Calleja <callejac1@gmail.com>
2022-07-22 19:39:50 -0400

    TDD

---
+++ src/practice2.js
@@ -0,0 +1,18 @@
+const addSubmission = (array, newName, newScore, newDate) => {
+  const newObject = {
+    name: newName,
+    score: newScore,
+    date: newDate,
+    passed: newScore >= 60,
+  };
+  array.push(newObject);
+};
+
+const deleteSubmissionByIndex = (array, index) => {
+  array.splice(index, 1);
+};
+
+module.exports = {
+  addSubmission: addSubmission,
+  deleteSubmissionByIndex: deleteSubmissionByIndex,
+};
